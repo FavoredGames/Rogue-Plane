3ae7794e7123bbf1bc9338b5fb98f31c2d52559e
FavoredGames <nathanbailey26@icloud.com>
2026-06-16 09:26:05 +1200

    Revert "Reapply "player bullets dont peirce""
    This reverts commit 8ad4de6c9ea87c41582ca18ce1f646dddbc7a4cf.

--- rogue-plane/scripts/bullet.gd
+++ rogue-plane/scripts/bullet.gd
@@ -5,11 +5,3 @@ var speed: float = 800.0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move_local_y(-speed * delta)
-
-
-
-func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("player"):
-		pass
-	else:
-		queue_free()
