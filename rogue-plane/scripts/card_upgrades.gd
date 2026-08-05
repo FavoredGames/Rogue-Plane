d635f663eaa1b38fbf55abba6ad85c8354b4a1e5
FavoredGames <nathanbailey26@icloud.com>
2026-08-06 08:49:16 +1200

    card upgrades are randomised

--- rogue-plane/scripts/card_upgrades.gd
+++ rogue-plane/scripts/card_upgrades.gd
@@ -8,9 +8,9 @@ extends Control
 @export var attack_speed_increase_card: Button
 @export var add_mini_plane_card: Button
 @export var add_gun_card: Button
-var rng = RandomNumberGenerator.new()
-var card_list: Array = [health_increase_card, damage_increase_card, 
-attack_speed_increase_card, add_mini_plane_card, add_gun_card]
+#var rng = RandomNumberGenerator.new()
+#var card_list: Array = [health_increase_card, damage_increase_card, 
+#attack_speed_increase_card, add_mini_plane_card, add_gun_card]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,25 +26,29 @@ func _process(delta: float) -> void:
 func show_card(id):
 	if id == 1:
 		health_increase_card.visible = true
+		print("show health card")
+		health_increase_card.position = card_spawn_1.global_position
 	
 	if id == 2:
 		damage_increase_card.visible = true
-		
+		print("show damage card")
+
 	if id == 3:
 		attack_speed_increase_card.visible = true
-		
+		print("show atks card")
 	if id == 4:
 		add_mini_plane_card.visible = true
-		
+		print("show mini card")
 	if id == 5:
 		add_gun_card.visible = true
-	
+		print("show gun card")
 
 
 func show_upgrade_cards():
 	visible = true
-	var random_int = randi_range(0,5)
-	print("randint", random_int)
+	print("---=--------------------------")
+	var random_int = randi_range(1,5)
+	print("randint:", random_int)
 		
 	if random_int == 1:
 		show_card(1)
@@ -53,6 +57,47 @@ func show_upgrade_cards():
 	elif random_int == 3: 
 		show_card(3)
 	elif random_int == 4: 
+		show_card(4)
+	else: 
+		show_card(5)
+		
+	var random_int_2 = randi_range(1,5)
+	print("randint2:", random_int_2)
+	while random_int_2 == random_int:
+		random_int_2 = randi_range(1,5)
+		print("randint2:", random_int_2)
+		if random_int_2 != random_int:
+			break
+	
+	if random_int_2 == 1:
+		show_card(1)
+	elif random_int_2 == 2: 
+		show_card(2)
+	elif random_int_2 == 3: 
+		show_card(3)
+	elif random_int_2 == 4: 
+		show_card(4)
+	else: 
+		show_card(5)
+		
+	var random_int_3 = randi_range(1,5)
+	print("randint3:", random_int_3)
+	while random_int_3 == random_int or random_int_3 == random_int_2:
+		random_int_3 = randi_range(1,5)
+		print("randint3:", random_int_3)
+		print("hmmm")
+		if random_int_3 == random_int_2 or random_int_3 == random_int:
+			pass
+		else:
+			print("break")
+			break
+	if random_int_3 == 1:
+		show_card(1)
+	elif random_int_3 == 2: 
+		show_card(2)
+	elif random_int_3 == 3: 
+		show_card(3)
+	elif random_int_3 == 4: 
 		show_card(4)
 	else: 
 		show_card(5)
@@ -74,19 +119,32 @@ func show_upgrade_cards():
 
 func _on_health_increase_card_pressed() -> void:
 	SignalManager.increase_max_health_temporary.emit()
-	health_increase_card.visible = false
+	hide_buttons()
 	unpause()
 
 
 func _on_damage_increase_card_pressed() -> void:
 	SignalManager.increase_damage.emit()
-	damage_increase_card.visible = false
+	hide_buttons()
 	unpause()
 
 
 func _on_increase_attack_speed_pressed() -> void:
 	SignalManager.increase_attack_speed.emit()
-	attack_speed_increase_card.visible = false
+	hide_buttons()
+	unpause()
+
+
+
+func _on_add_mini_plane_pressed() -> void:
+	SignalManager.add_mini_plane.emit()
+	hide_buttons()
+	unpause()
+
+
+func _on_add_gun_pressed() -> void:
+	SignalManager.add_gun.emit()
+	hide_buttons()
 	unpause()
 
 
@@ -96,16 +154,13 @@ func unpause():
 	get_tree().paused = false
 
 
-func _on_add_mini_plane_pressed() -> void:
-	SignalManager.add_mini_plane.emit()
-	add_mini_plane_card.visible = false
-	unpause()
-
-
-func _on_add_gun_pressed() -> void:
-	SignalManager.add_gun.emit()
+func hide_buttons():
 	add_gun_card.visible = false
-	unpause()
+	add_mini_plane_card.visible = false
+	attack_speed_increase_card.visible = false
+	damage_increase_card.visible = false
+	health_increase_card.visible = false
+
 
 
 func _on_unpause_button_pressed() -> void:
